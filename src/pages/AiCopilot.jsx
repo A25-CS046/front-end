0b3502ac1bcadb7722bdb6bb0490d8e681aa380f
Vania <221110551@students.mikroskil.ac.id>
2025-12-06 11:54:34 +0700

    feat: connect AiCopilot with copilot API service

--- src/pages/AiCopilot.jsx
+++ src/pages/AiCopilot.jsx
@@ -1,13 +1,17 @@
-import React, { useRef, useState } from "react";
-import { SendHorizonal, Bot, User } from "lucide-react";
-import { MessageSquarePlus } from "lucide-react";
-import ChatMessage from "@/components/AIcopilot/ChatMessage";
+import React, { useRef, useState, useEffect } from "react";
+import { SendHorizonal, MessageSquarePlus } from "lucide-react";
+import UserChatBubble from "@/components/AIcopilot/UserChatBubble";
+import AIChatBubble from "@/components/AIcopilot/AiChatBubble";
 import { formatTime } from "@/utils/formatTime";
 import { useAutoResizeTextarea } from "@/hooks/useAutoResizeTextarea";
-import useShrinkHeader from "@/hooks/useShrinkHeader";
+import LoadingBubble from "@/components/AIcopilot/LoadingBubble";
+
+// API
+import { sendCopilotMessage } from "@/services/copilotApi";
 
 export default function AiCopilot() {
   const [input, setInput] = useState("");
+  const [isAnalyzing, setIsAnalyzing] = useState(false);
   const [messages, setMessages] = useState([
     {
       id: Date.now(),
@@ -16,30 +20,73 @@ export default function AiCopilot() {
       time: new Date(),
     },
   ]);
-  // const [isScrolled, setScrolled] = useState(false);
   const textareaRef = useAutoResizeTextarea(input);
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   const suggestedQuestions = [
     "Tampilkan mesin yang memerlukan pemeliharaan minggu ini",
     "Tampilkan status untuk mesin nomor #",
-    "Berikan rekomendasi perbaikan untuk mesin saya",
-    "Tampilkan tren suhu mesin dalam 24 jam terakhir",
+    "Tampilkan riwayat pemeliharaan mesin nomor #",
+    "Ada berapa mesin yang sedang rusak saat ini?",
   ];
 
-  // useShrinkHeader(setScrolled);
-
-  const handleSend = () => {
+  const handleSend = async () => {
     if (!input.trim()) return;
 
-    const newMessage = {
+    const userMessage = {
       id: Date.now(),
       sender: "user",
       message: input,
       time: new Date(),
     };
+    setMessages((prev) => [...prev, userMessage]);
 
-    setMessages((prev) => [...prev, newMessage]);
+    const userInput = input;
     setInput("");
+    setIsAnalyzing(true);
+
+    try {
+      const result = await sendCopilotMessage(userInput);
+      console.log(result);
+      const aiMessage = {
+        id: Date.now(),
+        sender: "ai",
+        message: result.data.reply,
+        time: new Date(),
+      };
+
+      setMessages((prev) => [...prev, aiMessage]);
+    } catch (error) {
+      console.log(error);
+      const errorMessage = {
+        id: Date.now(),
+        sender: "ai",
+        message: "Error: Gagal memproses pertanyaan.",
+        time: new Date(),
+      };
+      setMessages((prev) => [...prev, errorMessage]);
+    }
+
+    setIsAnalyzing(false);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSend();
+    }
+  };
+
+  const handleSuggestedQuestion = (question) => {
+    setInput(question);
   };
 
   const handleNewChat = () => {
@@ -47,109 +94,110 @@ export default function AiCopilot() {
       {
         id: Date.now(),
         sender: "ai",
-        message: "Halo! Ada yang bisa saya bantu hari ini?",
+        message:
+          "Halo! Saya dapat membantu Anda menganalisis data mesin, performa mesin, dan lainnya. apa yang ingin anda ketahui?",
         time: new Date(),
       },
     ]);
   };
 
   return (
-    <div className="flex flex-col h-screen w-full dark:bg-slate-900">
-      {/* Header */}
-      <div className="sticky top-0 flex flex-row justify-between items-center px-6 py-4 bg-base-100">
-        <div className="flex flex-col">
-          <h1 className="text-xl text-primary dark:text-emerald-300">
-            AI Copilot
-          </h1>
-          <p className="text-sm text-base-content dark:text-gray-300">
-            Ask questions and get predictive insights powered by machine
-            learning
-          </p>
-        </div>
-        <button
-          className="btn btn-ghost border border-gray-300 dark:border-gray-700 gap-2"
-          onClick={handleNewChat}
-        >
-          <MessageSquarePlus className="w-4 h-4" />
-          New Chat
-        </button>
-
-        {/* {!isScrolled && (
-          <p className="text-sm text-base-content dark:text-gray-300">
-            Ask questions and get predictive insights powered by machine
-            learning
-          </p>
-        )} */}
+    <div className="flex flex-col h-[calc(100vh-64px)] lg:h-screen w-full p-6 space-y-3 dark:bg-slate-950">
+      {/* Page Header */}
+      <div className="px-1">
+        <h1 className="text-xl font-semibold text-primary dark:text-emerald-300">
+          AI COPILOT
+        </h1>
+        <p className="text-gray-600 dark:text-gray-400 text-sm">
+          Ask questions and get predictive insights powered by machine learning
+        </p>
       </div>
 
-      {/* Chat Area */}
-      <div className="flex-1 overflow-y-auto space-y-4 py-4 px-4 md:px-20 pb-50">
-        {messages.map((msg) => (
-          <ChatMessage
-            key={msg.id}
-            sender={msg.sender}
-            message={msg.message}
-            time={formatTime(msg.time)}
-          />
-        ))}
-      </div>
-
-      {/* Suggesstion */}
-      {messages.length === 1 && (
-        <div className="px-5 md:px-30 pb-3 md:pb-4">
-          <div className="mx-auto">
-            <p className="text-sm mb-2 md:mb-3 text-gray-600 dark:text-gray-400">
-              Pertanyaan yang disarankan:
+      {/* Chat Container */}
+      <div className="flex flex-col flex-1 min-h-0 bg-base-200 dark:bg-slate-900 rounded-2xl border border-gray-300 dark:border-slate-700 py-2">
+        {/* Chat Top Area */}
+        <div className="flex justify-between items-center px-6 pb-2 border-b border-gray-300 dark:border-slate-700">
+          <div>
+            <h2 className="text-md text-primary dark:text-emerald-300">
+              AEGIS ASSISTANT
+            </h2>
+            <p className="text-xs text-gray-600 dark:text-gray-400">
+              Online • Ready to help
             </p>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-              {suggestedQuestions.map((question, index) => (
-                <button
-                  key={index}
-                  onClick={() => {
-                    setInput(question);
-                  }}
-                  className="
-              text-left py-3 text-sm px-4
-              border rounded-xl transition-all
-              border-gray-300 text-gray-700 hover:bg-blue-50 hover:border-blue-300
-              dark:border-slate-700 dark:text-gray-300 dark:hover:bg-slate-800 dark:hover:border-cyan-600
-            "
-                >
-                  {question}
-                </button>
-              ))}
-            </div>
           </div>
-        </div>
-      )}
 
-      {/* Input Bar */}
-      <div className="sticky bottom-0 left-0 right-0 border-t py-3 px-4 md:px-20 border-gray-200 dark:bg-base-100 dark:border-gray-700">
-        <div className="mx-auto">
-          <div className="flex gap-3 h-auto items-end">
+          <button
+            onClick={handleNewChat}
+            className="btn btn-sm btn-ghost text-md border border-gray-300 dark:border-slate-700 text-gray-700 dark:text-gray-300 hover:bg-slate-800 px-5 py-3"
+          >
+            <MessageSquarePlus className="w-5 h-5 mr-1" /> New Chat
+          </button>
+        </div>
+
+        {/* Chat Messages Area */}
+        <div className="flex-1 overflow-y-auto mt-3 space-y-4 px-6">
+          {messages.map((msg) =>
+            msg.sender === "user" ? (
+              <UserChatBubble
+                key={msg.id}
+                message={msg.message}
+                time={formatTime(msg.time)}
+              />
+            ) : (
+              <AIChatBubble
+                key={msg.id}
+                message={msg.message}
+                time={formatTime(msg.time)}
+              />
+            )
+          )}
+
+          {isAnalyzing && <LoadingBubble />}
+
+          <div ref={messagesEndRef}></div>
+
+          {/* Suggesstion */}
+          {messages.length === 1 && (
+            <section className="px-3 pb-5">
+              <p className="text-sm mb-3 text-gray-600 dark:text-gray-400">
+                QUICK QUESTIONS
+              </p>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                {suggestedQuestions.map((question, index) => (
+                  <button
+                    key={index}
+                    onClick={() => handleSuggestedQuestion(question)}
+                    className="text-left text-sm py-2 px-4 border border-gray-300 dark:border-slate-700 rounded-xl transition-all hover:bg-blue-50 hover:border-blue-300 dark:hover:bg-slate-800 dark:hover:border-cyan-600 text-gray-700 dark:text-gray-300"
+                  >
+                    {question}
+                  </button>
+                ))}
+              </div>
+            </section>
+          )}
+        </div>
+
+        {/* Chat Input Area*/}
+        <div className="border-t border-gray-300 dark:border-slate-700 px-6 py-4">
+          <div className="flex gap-3 items-end">
             <textarea
+              ref={textareaRef}
               value={input}
               onChange={(e) => setInput(e.target.value)}
-              ref={textareaRef}
-              placeholder="Ask anything..."
+              onKeyDown={handleKeyPress}
               rows={1}
-              className="flex-1 resize-none rounded-2xl py-2 pl-3 max-h-[200px] overflow-y-hidden overflow-x-hidden  whitespace-pre-wrap border input bg-white border-gray-300 placeholder-gray-400 focus:border-blue-500
-                   dark:bg-slate-800 dark:border-slate-700 dark:text-white dark:placeholder-gray-500 dark:focus:border-cyan-500"
+              placeholder="Ask anything..."
+              className="flex-1 textarea textarea-bordered rounded-xl resize-none max-h-[150px] py-2 px-3 dark:bg-slate-800 dark:border-slate-700"
             />
             <button
-              className="flex btn bg-linear-to-r from-blue-500 to-cyan-500 hover:from-blue-600 hover:to-cyan-600 h-min-3 rounded-lg shadow-lg disabled:opacity-50 disabled:cursor-not-allowed"
-              onClick={handleSend}
               disabled={!input.trim()}
+              onClick={handleSend}
+              className="btn btn-primary text-white disabled:opacity-40"
             >
               <SendHorizonal className="w-5 h-5" />
             </button>
           </div>
-
-          <p className="text-xs mt-2 text-gray-400 dark:text-gray-500">
-            Tekan <b>Enter</b> untuk mengirim pesan. Tekan <b>Shift + Enter</b>{" "}
-            untuk memulai paragraf yang baru.
-          </p>
         </div>
       </div>
     </div>
