9a5de876da6db7dc0bcdd5c9695aa4509ef4dfec
Vania <221110551@students.mikroskil.ac.id>
2025-12-07 09:42:31 +0700

    feat: add history feature

--- src/pages/AiCopilot.jsx
+++ src/pages/AiCopilot.jsx
@@ -1,25 +1,38 @@
 import React, { useRef, useState, useEffect } from "react";
-import { SendHorizonal, MessageSquarePlus } from "lucide-react";
-import UserChatBubble from "@/components/AIcopilot/UserChatBubble";
-import AIChatBubble from "@/components/AIcopilot/AiChatBubble";
-import { formatTime } from "@/utils/formatTime";
+import { MessageSquarePlus } from "lucide-react";
 import { useAutoResizeTextarea } from "@/hooks/useAutoResizeTextarea";
-import LoadingBubble from "@/components/AIcopilot/LoadingBubble";
+import { getSessionId } from "@/utils/session";
+
+// Component
+import { Button } from "@/components/ui/Button";
+import ChatContainerTabs from "@/components/AIcopilot/ChatContainerTabs";
+import HistoryChatTab from "@/components/AIcopilot/HistoryChatContent";
+import CurrentChatContent from "@/components/AIcopilot/CurrentChatContent";
 
 // API
 import { sendCopilotMessage } from "@/services/copilotApi";
+import ChatContainerInput from "@/components/AIcopilot/ChatContainerInput";
 
 export default function AiCopilot() {
+  const sessionId = getSessionId();
   const [input, setInput] = useState("");
   const [isAnalyzing, setIsAnalyzing] = useState(false);
+  const [activeTab, setActiveTab] = useState("current");
   const [messages, setMessages] = useState([
     {
       id: Date.now(),
       sender: "ai",
       message: "Halo! Apa yang bisa saya bantu hari ini?",
-      time: new Date(),
+      time: Date.now(),
     },
   ]);
+  const [history, setHistory] = useState(() => {
+    const saved = localStorage.getItem("chatHistory");
+    return saved ? JSON.parse(saved) : [];
+  });
+  const [isLoadingHistory, setIsLoadingHistory] = useState(false);
+  const [currentSessionId, setCurrentSessionId] = useState(null);
+
   const textareaRef = useAutoResizeTextarea(input);
   const messagesEndRef = useRef(null);
 
@@ -49,13 +62,13 @@ export default function AiCopilot() {
     };
     setMessages((prev) => [...prev, userMessage]);
 
-    const userInput = input;
+    // const userInput = input;
     setInput("");
     setIsAnalyzing(true);
 
     try {
-      const result = await sendCopilotMessage(userInput);
-      console.log(result);
+      const result = await sendCopilotMessage(userMessage.message);
+
       const aiMessage = {
         id: Date.now(),
         sender: "ai",
@@ -90,115 +103,143 @@ export default function AiCopilot() {
   };
 
   const handleNewChat = () => {
+    if (currentSessionId) saveSession(messages, currentSessionId);
+
+    const newId = crypto.randomUUID();
+    setCurrentSessionId(newId);
+
+    setActiveTab("current");
+
     setMessages([
       {
         id: Date.now(),
         sender: "ai",
         message:
-          "Halo! Saya dapat membantu Anda menganalisis data mesin, performa mesin, dan lainnya. apa yang ingin anda ketahui?",
-        time: new Date(),
+          "Halo! Saya dapat membantu Anda menganalisis data mesin, performa mesin, dan lainnya. Apa yang ingin anda ketahui?",
+        time: Date.now(),
       },
     ]);
+  };
+
+  const saveSession = (messages, sessionIdOverride) => {
+    if (messages.length < 2) return;
+
+    const history = JSON.parse(localStorage.getItem("chatHistory") || "[]");
+
+    const id = sessionIdOverride || crypto.randomUUID();
+
+    const session = {
+      id: id,
+      title: messages[1]?.message || "New Conversation",
+      messages: messages,
+      createdAt: Date.now(),
+    };
+
+    const exists = history.find((s) => s.id === id);
+
+    let updated;
+    if (exists) {
+      updated = history.map((s) => (s.id === id ? session : s));
+    } else {
+      updated = [...history, session];
+    }
+
+    localStorage.setItem("chatHistory", JSON.stringify(updated));
+    setHistory(updated);
+  };
+
+  const deleteSession = (sessionId) => {
+    const updatedHistory = history.filter((s) => s.id !== sessionId);
+    localStorage.setItem("chatHistory", JSON.stringify(updatedHistory));
+    setHistory(updatedHistory);
+
+    if (currentSessionId === sessionId) {
+      setCurrentSessionId(null);
+      setMessages([
+        {
+          id: Date.now(),
+          sender: "ai",
+          message: "Halo! Apa yang bisa saya bantu hari ini?",
+          time: Date.now(),
+        },
+      ]);
+      setActiveTab("current");
+    }
   };
 
   return (
     <div className="flex flex-col h-[calc(100vh-64px)] lg:h-screen w-full p-6 space-y-3 dark:bg-slate-950">
       {/* Page Header */}
-      <div className="px-1">
-        <h1 className="text-xl font-semibold text-primary dark:text-emerald-300">
-          AI COPILOT
-        </h1>
-        <p className="text-gray-600 dark:text-gray-400 text-sm">
-          Ask questions and get predictive insights powered by machine learning
-        </p>
+      <div className="flex px-1 justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-xl font-semibold text-primary dark:text-emerald-300">
+            AI COPILOT
+          </h1>
+          <p className="hidden md:block text-gray-600 dark:text-gray-400 text-sm">
+            Ask questions and get predictive insights powered by machine
+            learning
+          </p>
+        </div>
+        <Button
+          onClick={handleNewChat}
+          className="flex items-center gap-2 px-4 py-2 
+           bg-blue-600 dark:bg-emerald-600
+           text-white 
+           border border-blue-600 dark:border-emerald-600
+           hover:bg-blue-700 dark:hover:bg-emerald-700
+           hover:scale-103 transition-all duration-200 
+           "
+        >
+          <MessageSquarePlus className="w-5 h-5" />
+          <span>New Chat</span>
+        </Button>
       </div>
 
       {/* Chat Container */}
-      <div className="flex flex-col flex-1 min-h-0 bg-base-200 dark:bg-slate-900 rounded-2xl border border-gray-300 dark:border-slate-700 py-2">
-        {/* Chat Top Area */}
-        <div className="flex justify-between items-center px-6 pb-2 border-b border-gray-300 dark:border-slate-700">
-          <div>
-            <h2 className="text-md text-primary dark:text-emerald-300">
-              AEGIS ASSISTANT
-            </h2>
-            <p className="text-xs text-gray-600 dark:text-gray-400">
-              Online • Ready to help
-            </p>
-          </div>
+      <div className="flex flex-col flex-1 min-h-0 bg-base-200 dark:bg-slate-900 rounded-2xl border border-gray-300 dark:border-slate-700">
+        <div className="relative">
+          <ChatContainerTabs
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
 
-          <button
-            onClick={handleNewChat}
-            className="btn btn-sm btn-ghost text-md border border-gray-300 dark:border-slate-700 text-gray-700 dark:text-gray-300 hover:bg-slate-800 px-5 py-3"
-          >
-            <MessageSquarePlus className="w-5 h-5 mr-1" /> New Chat
-          </button>
-        </div>
-
-        {/* Chat Messages Area */}
-        <div className="flex-1 overflow-y-auto mt-3 space-y-4 px-6">
-          {messages.map((msg) =>
-            msg.sender === "user" ? (
-              <UserChatBubble
-                key={msg.id}
-                message={msg.message}
-                time={formatTime(msg.time)}
-              />
-            ) : (
-              <AIChatBubble
-                key={msg.id}
-                message={msg.message}
-                time={formatTime(msg.time)}
-              />
-            )
-          )}
-
-          {isAnalyzing && <LoadingBubble />}
-
-          <div ref={messagesEndRef}></div>
-
-          {/* Suggesstion */}
-          {messages.length === 1 && (
-            <section className="px-3 pb-5">
-              <p className="text-sm mb-3 text-gray-600 dark:text-gray-400">
-                QUICK QUESTIONS
-              </p>
-
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-                {suggestedQuestions.map((question, index) => (
-                  <button
-                    key={index}
-                    onClick={() => handleSuggestedQuestion(question)}
-                    className="text-left text-sm py-2 px-4 border border-gray-300 dark:border-slate-700 rounded-xl transition-all hover:bg-blue-50 hover:border-blue-300 dark:hover:bg-slate-800 dark:hover:border-cyan-600 text-gray-700 dark:text-gray-300"
-                  >
-                    {question}
-                  </button>
-                ))}
-              </div>
-            </section>
+          {isLoadingHistory && (
+            <div className="absolute bottom-0 left-0 w-full h-1 overflow-hidden bg-gray-100 dark:bg-slate-800">
+              <div className="w-full h-full bg-linear-to-r from-blue-600 to-cyan-600 animate-loading-bar"></div>
+            </div>
           )}
         </div>
 
-        {/* Chat Input Area*/}
-        <div className="border-t border-gray-300 dark:border-slate-700 px-6 py-4">
-          <div className="flex gap-3 items-end">
-            <textarea
-              ref={textareaRef}
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              onKeyDown={handleKeyPress}
-              rows={1}
-              placeholder="Ask anything..."
-              className="flex-1 textarea textarea-bordered rounded-xl resize-none max-h-[150px] py-2 px-3 dark:bg-slate-800 dark:border-slate-700"
-            />
-            <button
-              disabled={!input.trim()}
-              onClick={handleSend}
-              className="btn btn-primary text-white disabled:opacity-40"
-            >
-              <SendHorizonal className="w-5 h-5" />
-            </button>
-          </div>
-        </div>
+        {activeTab === "current" && (
+          <CurrentChatContent
+            messages={messages}
+            isAnalyzing={isAnalyzing}
+            messagesEndRef={messagesEndRef}
+            suggestedQuestions={suggestedQuestions}
+            handleSuggestedQuestion={handleSuggestedQuestion}
+          />
+        )}
+
+        {activeTab === "history" && (
+          <HistoryChatTab
+            history={history}
+            setCurrentSessionId={setCurrentSessionId}
+            setMessages={setMessages}
+            setActivateTab={setActiveTab}
+            setIsLoadingHistory={setIsLoadingHistory}
+            onDeleteSession={deleteSession}
+          />
+        )}
+
+        {activeTab === "current" && (
+          <ChatContainerInput
+            input={input}
+            setInput={setInput}
+            handleSend={handleSend}
+            handleKeyPress={handleKeyPress}
+            textareaRef={textareaRef}
+          />
+        )}
       </div>
     </div>
   );
